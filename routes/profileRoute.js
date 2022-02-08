694fa6bc1d0016160338d4105fe9cc8a68f6cd03
Juannjuu <juanrahmadhika@gmail.com>
2022-02-08 21:40:04 +0700

    init development-profiles

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {register, editProfile, getProfile} = require("../controllers/profileController")
+const {register, editProfile, getProfile, changePassword} = require("../controllers/profileController")
 const {isLogin} = require("../middlewares/auth")
 const {registerValidation} = require("../middlewares/authValidation")
 
 router.post("/register", registerValidation, register)
-router.post("/", isLogin, editProfile)
 router.post("/", isLogin, getProfile)
+router.put("/", isLogin, editProfile)
+router.put("/changePassword", isLogin, changePassword)
 
 module.exports = router
