d3c95066cd8c6afe2342f6151db3a0322115a0f4
Juannjuu <juanrahmadhika@gmail.com>
2022-02-08 21:33:34 +0700

    ! fix index routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const authRoute = require('./authRoute');
+const profileRoute = require('./profileRoute');
+const boardRoute = require('./boardRoute');
+const teamRoute = require('./teamRoute');
+const cardRoute = require('./cardRoute');
+
+router.use("/auth", authRoute);
+router.use("/profile", profileRoute);
+router.use("/board", boardRoute);
+router.use("/team", teamRoute);
+router.use("/card", cardRoute);
 
 module.exports = router
