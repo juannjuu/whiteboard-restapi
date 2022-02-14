e484479165bd329231d58d7ee8334b513ebf93db
Juannjuu <juanrahmadhika@gmail.com>
2022-02-14 22:27:42 +0700

    ! fix error

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
+const passport = require('passport')
 const app = express()
 const port = process.env.PORT || 5000
 const connect = require('./config')
