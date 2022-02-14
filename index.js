d728d0dc433b45b5ff64a41b7a9a88e91382131b
Juannjuu <juanrahmadhika@gmail.com>
2022-02-14 22:29:08 +0700

    ! fix error

--- index.js
+++ index.js
@@ -18,15 +18,6 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get(
-    "/api/v1/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/api/v1/auth/google" }),
-    (req, res) => {
-        console.log(req.user);
-        res.redirect("/user");
-    }
-);
-
 app.listen(port, () => {
     console.log('Server is running at port ', port)
 })
