71fc148dd24494b4ace36c0d43de3bc4036697f5
Juannjuu <juanrahmadhika@gmail.com>
2022-03-01 15:48:45 +0700

    + add invite members

--- routes/boardRoute.js
+++ routes/boardRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {isLogin} = require("../middlewares/auth")
-const {getBoards, getBoardDetail, createList, createBoard, getBoardMembers, archiveList} = require("../controllers/boardController")
+const {getBoards, getBoardDetail, createList, createBoard, getBoardMembers, archiveList, getOneUser, inviteMembers, copyList} = require("../controllers/boardController")
 
 router.get("/:teamId", isLogin, getBoards)
 router.get("/board/:boardId", isLogin, getBoardDetail)
@@ -9,5 +9,8 @@ router.post("/:boardId/list", isLogin, createList)
 router.post("/:teamId", isLogin, createBoard)
 router.get("/members/:boardId", isLogin, getBoardMembers)
 router.put("/:listId/archive", isLogin, archiveList)
+router.get("/members/:boardId/member", isLogin, getOneUser)
+router.put("/members/:boardId/member", isLogin, inviteMembers)
+router.post("/:listId/copy", isLogin, copyList)
 
 module.exports = router
