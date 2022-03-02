d614e8e4d567cc58e82caa83653fef8519affa21
Juannjuu <juanrahmadhika@gmail.com>
2022-03-02 11:44:13 +0700

    + add mail-sender utils

--- utils/mail-sender.js
+++ utils/mail-sender.js
@@ -1,29 +1,30 @@
 const nodemailer = require("nodemailer");
 
-module.exports = async(email, subject, content) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_SMTP,
-                pass: process.env.PASSWORD_EMAIL,
-            },
-        });
-        let info = await transporter.sendMail({
-            from: '"Whiteboard" <no-reply@whiteboard-app.com>',
-            to: email,
-            subject: subject,
-            text: "",
-            html: content,
-        });
-
-        console.log("Message sent: %s", info.messageId);
-
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    } catch (error) {
-        console.log(error);
+module.exports = {
+    sendEmail : async(email, subject, content) => {
+        try {
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.NODEMAILER_USER,
+                    pass: process.env.NODEMAILER_PASS,
+                },
+            });
+            let info = await transporter.sendMail({
+                from: '"Whiteboard" <no-reply@whiteboard-app.com>',
+                to: email,
+                subject: subject,
+                html: content,
+            });
+    
+            console.log("Message sent: %s", info.messageId);
+    
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } catch (error) {
+            console.log(error);
+        }
     }
-};
+}
