364075fe27dad42461acba21d8dc2260657a231b
Juannjuu <juanrahmadhika@gmail.com>
2022-03-02 11:38:38 +0700

    + fix board routes

--- routes/boardRoute.js
+++ routes/boardRoute.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {isLogin} = require("../middlewares/auth")
-const {getBoards, getBoardDetail, createList, createBoard, getBoardMembers, archiveList, getOneUser, inviteMembers, copyList} = require("../controllers/boardController")
+const BoardController = require("../controllers/boardController")
 
-router.get("/:teamId", isLogin, getBoards)
-router.get("/board/:boardId", isLogin, getBoardDetail)
-router.post("/:boardId/list", isLogin, createList)
-router.post("/:teamId", isLogin, createBoard)
-router.get("/members/:boardId", isLogin, getBoardMembers)
-router.put("/:listId/archive", isLogin, archiveList)
-router.get("/members/:boardId/member", isLogin, getOneUser)
-router.put("/members/:boardId/member", isLogin, inviteMembers)
-router.post("/:listId/copy", isLogin, copyList)
+router.get("/:teamId", isLogin, BoardController.getBoards)
+router.get("/board/:boardId", isLogin, BoardController.getBoardDetail)
+router.post("/:boardId/list", isLogin, BoardController.createList)
+router.post("/:teamId", isLogin, BoardController.createBoard)
+router.get("/members/:boardId", isLogin, BoardController.getBoardMembers)
+router.put("/:listId/archive", isLogin, BoardController.archiveList)
+router.get("/members/:boardId/member", isLogin, BoardController.getOneUser)
+router.put("/members/:boardId/member", isLogin, BoardController.inviteMembers)
+router.post("/:listId/copy", isLogin, BoardController.copyList)
 
 module.exports = router
