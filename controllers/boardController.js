aa58c3c4e669fada8c0d7fde0c3efbf42652a698
Juannjuu <juanrahmadhika@gmail.com>
2022-03-03 16:51:20 +0700

    ! fix html nodemailer

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -5,6 +5,7 @@ const Profile = require("../models/profile")
 const Card = require("../models/card")
 const Joi = require("joi");
 const errorHandler = require('../utils/error-handler')
+const {sendEmail} = require("../utils/mail-sender")
 
 module.exports = {
     getBoards : async (req, res) => {
@@ -193,13 +194,6 @@ module.exports = {
                     result: {}
                 })
             }
-            // const profile = await Profile.findOne({userId : user._id})
-            // if(!profile){
-            //     return res.status(404).json({
-            //         status: "Not Found",
-            //         message: "Profile is Not Found",
-            //     })
-            // }
             const userResult = {
                 email : user.email,
                 name : user.name,
@@ -219,6 +213,7 @@ module.exports = {
         const body = req.body
         var arrMember = []
         try {
+            const userLogin = await User.findOne({_id : req.user.id})
             const user = await User.find({email : body.email}).populate({
                 path : "profileId",
                 select : "image"
@@ -245,16 +240,84 @@ module.exports = {
                     profileId : user[i].profileId
                 }
                 arrMember.push(member)
+                sendEmail(
+                    user[i].email, 
+                    "Whiteboard Invitation",
+                    `
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <!--100% body table-->
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                          <a href="https://whiteboardglints.herokuapp.com/" title="logo" target="_blank">
+                            <img width="200" src="https://res.cloudinary.com/dry2yqm3h/image/upload/v1646299280/image/whiteboard/whiteboard-logo_xbavgv.png" title="logo" alt="logo">
+                          </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You are invited by <b>${userLogin.name}</b> to join a <b>${getBoard.title}</b> board</h1>
+                                        <span
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                            Someone is inviting you to join a board. To confirm this invitation, please click the
+                                            following link below and follow the instructions.
+                                        </p>
+                                        <a href="https://whiteboardglints.herokuapp.com/"
+                                            style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Confirm Invitation</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>whiteboardproduct</strong></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <!--/100% body table-->
+</body>   `
+                    )
             }
-            const updateMember = await Board.findOneAndUpdate({_id : boardId}, {
-                $push: {members : arrMember}
-            }, {new: true})
-            if(!updateMember){
-                return res.status(500).json({
-                    status: "Internal Server Error",
-                    message: "Invite members is Failed",
-                })
-            }
+            // const updateMember = await Board.findOneAndUpdate({_id : boardId}, {
+            //     $push: {members : arrMember}
+            // }, {new: true})
+            // if(!updateMember){
+            //     return res.status(500).json({
+            //         status: "Internal Server Error",
+            //         message: "Invite members is Failed",
+            //     })
+            // }
             res.status(201).json({
                 status: "OK",
                 message: "Invite members is success",
