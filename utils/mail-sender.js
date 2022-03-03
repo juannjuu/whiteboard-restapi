c71f246b7050a6ac4434833568dc10ec93954e00
Juannjuu <juanrahmadhika@gmail.com>
2022-03-03 17:05:24 +0700

    fix mail-sender

--- utils/mail-sender.js
+++ utils/mail-sender.js
@@ -1,28 +1,30 @@
 const nodemailer = require("nodemailer");
 
-exports.sendEmail = async(to, subject, html) => {
-    try {
-        let transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.NODEMAILER_USER,
-                pass: process.env.NODEMAILER_PASS,
-            },
-        });
-
-
-        let info = await transporter.sendMail({
-            from: process.env.NODEMAILER_USER,
-            to,
-            subject,
-            html: html
-        });
-
-        console.log('Message sent: %s', info.messageId)
-
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-
-    } catch (error) {
-        console.log(error);
+module.exports = {
+    sendEmail : async(email, subject, content) => {
+        try {
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.NODEMAILER_USER,
+                    pass: process.env.NODEMAILER_PASS,
+                },
+            });
+            let info = await transporter.sendMail({
+                from: '"Whiteboard" <no-reply@whiteboard-app.com>',
+                to: email,
+                subject: subject,
+                html: content,
+            });
+    
+            console.log("Message sent: %s", info.messageId);
+    
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
