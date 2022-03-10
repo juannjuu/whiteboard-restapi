1237be43f5f51754f72d348087568a6af295650a
Khalid Al Asad <katakgendong@gmail.com>
2022-03-10 23:31:48 +0700

    fixing route for team detail

--- routes/teamRoute.js
+++ routes/teamRoute.js
@@ -6,6 +6,6 @@ const TeamController = require('../controllers/teamController')
 router.get('/', isLogin, TeamController.getTeam)
 router.get('/:teamId', isLogin, TeamController.getOneTeam)
 router.post('/', isLogin, TeamController.createTeam)
-router.get('/detail', isLogin, TeamController.getDetailTeam)
+router.get('/detail/team', isLogin, TeamController.getDetailTeam)
 
 module.exports = router
