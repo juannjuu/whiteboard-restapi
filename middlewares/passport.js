620b72adc5c0e22ca006678b9871ef71bca9ab96
Juannjuu <juanrahmadhika@gmail.com>
2022-03-10 21:41:52 +0700

    remove passport.js from middleware

--- middlewares/passport.js
+++
@@ -1,9 +0,0 @@
-// app.get("/auth/google", passport.authenticate("google"));
-
-// app.get(
-//     "/auth/google/callback",
-//     passport.authenticate("google", { failureRedirect: "/login" }),
-//     function(req, res) {
-//         res.redirect("/");
-//     }
-// );
