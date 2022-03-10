6c3dbde8857f906ffedcc9598a64ade7c3abf730
Juannjuu <juanrahmadhika@gmail.com>
2022-03-10 22:48:57 +0700

    ! fix syntax profileRoute.js

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {editProfile, getProfile, changePassword, changeEmail} = require("../controllers/profileController")
+const ProfileController = require("../controllers/profileController")
 const {isLogin} = require("../middlewares/auth")
 const {uploadCloud} = require("../middlewares/cloudUpload")
-const {editProfileValidation, changeEmailValidation} = require("../middlewares/profileValidation")
+const {editProfileValidation} = require("../middlewares/profileValidation")
 
-router.get("/", isLogin, getProfile)
-router.put("/", isLogin, editProfileValidation ,uploadCloud("image"), editProfile)
-router.put("/changepassword", isLogin, changePassword)
-router.put("/changemail", isLogin, changeEmailValidation, changeEmail)
+router.get("/", isLogin, ProfileController.getProfile)
+router.put("/", isLogin, editProfileValidation ,uploadCloud("image"), ProfileController.editProfile)
+router.put("/changepassword", isLogin, ProfileController.changePassword)
 
 module.exports = router
